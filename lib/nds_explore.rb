41ebbb4495a15286c6570e94a1d2c31c4fa28462
Kelly Becker <kelly.lynn.becker@gmail.com>
2019-12-19 00:38:10 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -2,14 +2,14 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
-def pretty_print_nds(nds)
+def pretty_print_nds
   pp nds
 end
 
 
 def print_first_directors_movie_titles
-  
-print "hello "
+
+
 
 end
 
